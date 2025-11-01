5b19bbdb4ac32b4f6b8b111aaf960628cd32d7dd
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2025-11-01 20:21:04 +0900

    コミット

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	site: 'https://gjtakahashi.github.io/gjtakahashi.github.io/'
+	site: 'https://gjtakahashi.github.io/'
 });
