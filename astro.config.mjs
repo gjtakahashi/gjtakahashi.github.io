793f7776bf96253e42813f7a71c88f5be6cf27d7
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2025-11-01 13:42:04 +0900

    Github Actions用のファイルをコミット

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'static',
+	site: 'https://gjtakahashi.github.io/gjtakahashi.github.io/',
+	base: '/gjtakahashi.github.io',
+});
